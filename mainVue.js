1546ced26ca5ed11f7cb04ba8ad2e64efc80cc37
Thanawat Yodnil <littleboycoding@gmail.com>
2020-04-15 22:37:20 +0700

    Fix notification request bug, Change icons

---
+++ mainVue.js
@@ -0,0 +1,108 @@
+Vue.component("text-box", {
+	props: ["notification"],
+	template:
+		"<div class='text'><slot></slot><br/><br/><warning :notification='notification'></warning></div>"
+});
+
+Vue.component("warning", {
+	props: ["notification"],
+	template:
+		"<span class='warning' v-if='notificationDenied'>We can't notify you because you aren't allow us to, Please change setting if you change your mind.</span>",
+	computed: {
+		notificationDenied() {
+			return this.notification === "denied" ? true : false;
+		}
+	}
+});
+
+Vue.component("help-text", {
+	data() {
+		return {
+			help: []
+		};
+	},
+	async created() {
+		let helpText = await fetch("helpText.txt").then(res =>
+			res.text()
+		);
+		this.help = helpText.split("\n");
+	},
+	computed: {
+		randomComputed() {
+			const randomIndex = Math.floor(
+				Math.random() * (this.help.length - 0.1)
+			);
+			return this.help[randomIndex];
+		}
+	},
+	template: '<span style="font-size: 2vw">> {{randomComputed}}</span>'
+});
+
+let vm = new Vue({
+	el: "#app",
+	async mounted() {
+		//assume that 1000 ms = 1 min for now...
+		const ms = 60000; //60000 = 1 min
+		if (!"Notification" in window) {
+			alert(
+				"Your browser doesn't not supported notification, So we can't send notify to you."
+			);
+		} else if (
+			Notification.permission !== "granted" &&
+			Notification.permission !== "denied"
+		) {
+			await Notification.requestPermission();
+		}
+		this.notificationState = Notification.permission;
+		setInterval(this.timeForward, ms);
+	},
+	data: {
+		timer: 0, //minute
+		alreadyNotify: false, //prevent notification to appear more than one
+		timeLimit: 30,
+		fetchedGif: {},
+		notificationState: ""
+	},
+	methods: {
+		async timeForward() {
+			if (this.timer < this.timeLimit) {
+				this.timer += 1;
+			} else if (
+				!this.alreadyNotify &&
+				Notification.permission === "granted"
+			) {
+				this.alreadyNotify = true;
+
+				//Get random lovely cat picture from thecatapi
+				const catapi = `https://api.thecatapi.com/v1/images/search`;
+				this.fetchedGif = await fetch(catapi)
+					.then(res => res.json())
+					.catch(err => "error");
+
+				let notification = new Notification(
+					"Take a Break",
+					{
+						image:
+							this.fetchedGif !==
+							"error"
+								? this
+										.fetchedGif[0][
+										"url"
+								  ]
+								: "",
+						body: `You have sat for ${this.timeLimit} mintues ! Get up take some break 🎉`
+					}
+				);
+			}
+		},
+		timeReset() {
+			this.timer = 0;
+			this.alreadyNotify = false;
+		}
+	},
+	computed: {
+		isTime() {
+			return this.timer == this.timeLimit ? true : false;
+		}
+	}
+});
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,40 +1,43 @@
 const version = "1";
 const cache_name = "Take a Break v" + version;
 let file_to_cache = [
-  ".",
-  "index.html",
-  "stylesheet.css",
-  "https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
-  "helpText.txt",
+	".",
+	"index.html",
+	"mainVue.js",
+	"stylesheet.css",
+	"https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
+	"helpText.txt"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(cache_name).then((cache) => cache.addAll(file_to_cache))
-  );
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches
+			.open(cache_name)
+			.then(cache => cache.addAll(file_to_cache))
+	);
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => {
-      if (res) {
-        return res;
-      }
-      return fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request).then(res => {
+			if (res) {
+				return res;
+			}
+			return fetch(event.request);
+		})
+	);
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== cache_name) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("activate", event => {
+	event.waitUntil(
+		caches.keys().then(keyList => {
+			return Promise.all(
+				keyList.map(key => {
+					if (key !== cache_name) {
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
 });
