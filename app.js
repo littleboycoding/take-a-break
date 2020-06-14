94cd4e683807356f6f6fdc7aa529ecae46e7e60f
Thanawat Yodnil <littleboycoding@gmail.com>
2020-06-14 12:17:58 +0700

    Update code style

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let registration;
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", async () => {
-    registration = await navigator.serviceWorker
-      .register("serviceWorker.js")
-      .then((reg) => reg);
-  });
+	window.addEventListener("load", async () => {
+		registration = await navigator.serviceWorker
+			.register("serviceWorker.js")
+			.then(reg => reg);
+	});
 }
